6dbe063dc88b4d232e4e114f7f5a138597b23871
xinlo <1311257306@qq.com>
2022-06-19 00:27:14 +0800

    更新bug

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import VueRouter from "vue-router";
-import router from './router'
+import axios from "axios";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from "axios";
-import Vuex from 'vuex'
-import store from './store'
+import Vue from 'vue';
+import VueRouter from "vue-router";
+import Vuex from 'vuex';
+import App from './App';
+import router from './router';
+import store from './store';
 
 Vue.prototype.axios = axios
 Vue.use(ElementUI)
